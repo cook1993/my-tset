9c4e3adf498c8a88f4ed50953bbed9d00efc468d
unknown <qyn342@163.com>
2020-02-16 22:06:30 +0800

    ui框架及路由的配置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Elementui from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from './router/router'
+Vue.use(Elementui)
+// 控制台信息更加详细
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
